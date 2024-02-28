0cadf41467b1424aac5639d824a8683969229789
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2024-02-28 17:12:45 +0100

    Logging exception to LogCat when not using Crashlytics

--- app/src/fdroid/java/lu/fisch/canze/classes/Crashlytics.java
+++ app/src/fdroid/java/lu/fisch/canze/classes/Crashlytics.java
@@ -1,9 +1,13 @@
 package lu.fisch.canze.classes;
 
+import android.util.Log;
+
 public class Crashlytics {
     public static void logException (Exception e) {
+        Log.e("CanZE", "An error occurred", e);
     }
 
-    public static void logString (String e) {
+    public static void logString (String m) {
+        Log.w("CanZE", m);
     }
 }
