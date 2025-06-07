0ee313f50362d254384f746baecba70896e6bf7a
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2025-06-07 21:59:04 +0200

    Properly using atomic boolean in StoppableThread

--- app/src/main/java/lu/fisch/canze/actors/StoppableThread.java
+++ app/src/main/java/lu/fisch/canze/actors/StoppableThread.java
@@ -1,30 +1,29 @@
 package lu.fisch.canze.actors;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /**
  * Created by robertfisch on 30.12.2016.
  */
 
 public class StoppableThread extends Thread {
 
-    private boolean stopped = false;
+    private final AtomicBoolean stopped = new AtomicBoolean(false);
 
     public StoppableThread(Runnable runnable) {
         super(runnable);
     }
 
-    public void start()
-    {
-        stopped=false;
+    public void start() {
+        stopped.getAndSet(false);
         super.start();
     }
 
-    public void tryToStop()
-    {
-        stopped=true;
+    public void tryToStop() {
+        stopped.getAndSet(true);
     }
 
-    public boolean isStopped()
-    {
-        return stopped;
+    public boolean isStopped() {
+        return stopped.get();
     }
 }
